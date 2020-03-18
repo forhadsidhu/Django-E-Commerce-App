152e0eb118d88efd8cb71de31f6ae0a7ee30516f
Sharif-Ullah-CUET <md.sharif.ullah.forhad@gmail.com>
2020-03-19 00:53:33 +0600

    all product code updated

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('',include('Shop.urls')),
 ]
 # t explicitly means that if it is on development mode and not in production mode!
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
